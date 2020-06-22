998667fcbbbe1dd8820a629ea7050fa9ecd962c6
Pro <Pro@hui.com>
2020-06-22 20:00:22 +0300

    вторая практика

--- scrips.js
+++ scrips.js
@@ -7,14 +7,29 @@ const MovieBD = {
     genres: [],
     privat: false
 };
+if (MovieBD.count < 10 ){
+    console.log('Просмотренно мало фильмов');
+} else if (MovieBD.count >= 10 && MovieBD.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (MovieBD.count > 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
-const a = prompt ('какой фильм?', ''),
-    b = prompt ('Оценка', ''),
-    c = prompt ('какой фильм?', ''),
-    d = prompt ('Оценка', '');
+let i = 0;
+do {
+    const a = prompt ('какой фильм?', ''),
+          b = prompt ('Оценка', '');
+if (a !=null && b !=null && a !='' && b !='' && a.length < 50){
+    MovieBD.movies[a] = b; 
+    console.log('done');
+}   else {
+    console.log('error');
+    i--;
+}
+i++;
+  }while (i < 2);
 
-MovieBD.movies[a] = b;
-MovieBD.movies[c] = d;
 
 console.log(MovieBD);
-console.log(MovieBD.pricat);
