a2dae29fe3b0b279699031e67f87b3236d2554d7
Pro <Pro@hui.com>
2020-06-24 23:03:28 +0300

    18 lessons

--- scrips.js
+++ scrips.js
@@ -1,7 +1,14 @@
 "use strict";
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+function start (){
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+            numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+    }
+}
+start ();
 
 const MovieBD = {
     count: numberOfFilms,
@@ -10,35 +17,50 @@ const MovieBD = {
     genres: [],
     privat: false
 };
-if (MovieBD.count < 10 ){
-    console.log('Просмотренно мало фильмов');
-} else if (MovieBD.count >= 10 && MovieBD.count <= 30) {
-    console.log('Вы классический зритель');
-} else if (MovieBD.count > 30){
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function rememberFilm(){
+    for (let i = 0; i < 2; i++){
+        const a = prompt ('какой фильм?', ''),
+              b = prompt ('Оценка', '');
+    if (a !=null && b !=null && a !='' && b !='' && a.length < 50){
+        MovieBD.movies[a] = b; 
+        console.log('done');
+    }   else {
+        console.log('error');
+        i--;
+    }
+    }
 }
 
-let i = 0;
-do {
-    const a = prompt ('какой фильм?', ''),
-          b = prompt ('Оценка', '');
-if (a !=null && b !=null && a !='' && b !='' && a.length < 50){
-    MovieBD.movies[a] = b; 
-    console.log('done');
-}   else {
-    console.log('error');
-    i--;
+rememberFilm();
+
+function level(){
+    if (MovieBD.count < 10 ){
+        console.log('Просмотренно мало фильмов');
+    } else if (MovieBD.count >= 10 && MovieBD.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (MovieBD.count > 30){
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
-i++;
-  }while (i < 2);
+level();
 
-
-console.log(MovieBD);
-
-function calc (a, b){
-    return (a+b);
+function showMeDB () {
+    if (MovieBD.privat == false) {
+        console.log(MovieBD);
+        
+    }
 }
-console.log(calc(5, 8));
+showMeDB(MovieBD.privat);
 
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++){
+        const gender = prompt(`Ваш любимый жанр под номером ${i}`);
+        MovieBD.genres[i-1] = gender;
+        
+    }
+}
+
+writeYourGenres();
